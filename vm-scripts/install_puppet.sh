d4254272dc72b8971e95fca5bbf580b1e5591b62
Bert Hajee <bert.hajee@enterprisemodules.com>
2022-11-03 14:08:25 +0100

    Start using EL 9

--- vm-scripts/install_puppet.sh
+++ vm-scripts/install_puppet.sh
@@ -1,4 +1,4 @@
-if [ -f /var/log/install_puppet.done ]
+if [ -f /opt/puppetlabs/bin/puppet ]
 then
   echo "Puppet already installed"
 else
@@ -11,10 +11,18 @@ else
     PACKAGE="puppet-agent"
   fi
   echo "Installing $PACKAGE"
-  rhel=$(awk -F'[ .]' 'NF==8{print $6} NF==9{print $7}' /etc/redhat-release)
-  yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet7/puppet7-release-el-${rhel}.noarch.rpm > /dev/null
-  yum install -y --nogpgcheck $PACKAGE
-  rpm -q git || yum install -y --nogpgcheck git
-
+  unamestr=`uname`
+  if [[ "$unamestr" == 'Linux' ]]; then
+    rhel=$(grep '^VERSION_ID' /etc/os-release | awk -F'[=."]' '{ print $3}')
+    if [ "$rhel" -eq "9" ]; then
+      cp /vagrant/vm-scripts/puppet7.repo /etc/yum.repos.d/
+    else
+      yum install -y --nogpgcheck https://yum.puppetlabs.com/puppet7/puppet7-release-el-${rhel}.noarch.rpm > /dev/null
+    fi
+    yum install -y --nogpgcheck $PACKAGE
+    rpm -q git || yum install -y --nogpgcheck git
+  elif [[ "$unamestr" == 'SunOS' ]]; then
+    pkg install -g file:///vagrant/modules/software/files/puppet-agent\@7.8.0\,5.11-1.i386.p5p puppet-agent
+  fi
   touch /var/log/install_puppet.done
 fi
